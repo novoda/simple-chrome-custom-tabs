3ec3acaa3b97b95198ffab4b7d6216cd54463d7e
Juanky Soriano <juanky@novoda.com>
2015-11-15 19:57:57 +0000

    Add TODOs

--- library/src/main/java/com/novoda/easycustomtabs/connection/EasyCustomTabsConnection.java
+++ library/src/main/java/com/novoda/easycustomtabs/connection/EasyCustomTabsConnection.java
@@ -1,10 +1,17 @@
 package com.novoda.easycustomtabs.connection;
 
 import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.customtabs.CustomTabsSession;
 
+import java.util.List;
+
+/**
+ * TODO Open implementation to allow {@link CustomTabsSession#mayLaunchUrl(Uri, Bundle, List)}
+ */
 public class EasyCustomTabsConnection implements Connection, ServiceConnectionCallback {
 
     private static final CustomTabsSession NULL_SESSION = null;
--- library/src/main/java/com/novoda/easycustomtabs/navigation/EasyCustomTabsIntentBuilder.java
+++ library/src/main/java/com/novoda/easycustomtabs/navigation/EasyCustomTabsIntentBuilder.java
@@ -70,6 +70,9 @@ public class EasyCustomTabsIntentBuilder {
         return this;
     }
 
+    /**
+     * TODO Allow intent creation without connection. Might be useful to re-customise a current session.
+     */
     public CustomTabsIntent createIntent() {
         if (!connection.isConnected()) {
             throw new DeveloperError("An active connection to custom tabs service is required for intent creation");
