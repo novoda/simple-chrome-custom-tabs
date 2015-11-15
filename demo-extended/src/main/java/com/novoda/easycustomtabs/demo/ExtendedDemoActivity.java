0ba87c7c0e75fea77181241f5d8636311b155b1e
Juanky Soriano <juanky@novoda.com>
2015-11-15 18:39:00 +0000

    Add animation transtions to extended demo

--- demo-extended/src/main/java/com/novoda/easycustomtabs/demo/ExtendedDemoActivity.java
+++ demo-extended/src/main/java/com/novoda/easycustomtabs/demo/ExtendedDemoActivity.java
@@ -10,6 +10,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.novoda.easycustomtabs.EasyCustomTabs;
 import com.novoda.easycustomtabs.navigation.EasyCustomTabsIntentBuilder;
