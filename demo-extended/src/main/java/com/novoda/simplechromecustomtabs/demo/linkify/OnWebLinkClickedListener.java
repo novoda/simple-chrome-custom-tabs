364644f6896d08f3780c14c96abe025f5b6b6879
Alex Styl <alex.styl@novoda.com>
2016-04-13 11:33:36 +0100

    Add doc on OnWebLinkClickedListener

--- demo-extended/src/main/java/com/novoda/simplechromecustomtabs/demo/linkify/OnWebLinkClickedListener.java
+++ demo-extended/src/main/java/com/novoda/simplechromecustomtabs/demo/linkify/OnWebLinkClickedListener.java
@@ -3,5 +3,10 @@ package com.novoda.simplechromecustomtabs.demo.linkify;
 import android.net.Uri;
 
 public interface OnWebLinkClickedListener {
+    /**
+     * Called when a Web link has been clicked
+     *
+     * @param url The uri to the link that has been pressed
+     */
     void onClick(Uri url);
 }
