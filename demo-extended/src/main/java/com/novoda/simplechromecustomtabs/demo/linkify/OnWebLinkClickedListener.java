0a78f607e29c05681e1b04bea2229f7e6002bdef
Alex Styl <alex.styl@novoda.com>
2016-04-13 14:51:20 +0100

    Rename parameter Uri variable name to uri instead of url

--- demo-extended/src/main/java/com/novoda/simplechromecustomtabs/demo/linkify/OnWebLinkClickedListener.java
+++ demo-extended/src/main/java/com/novoda/simplechromecustomtabs/demo/linkify/OnWebLinkClickedListener.java
@@ -6,7 +6,7 @@ public interface OnWebLinkClickedListener {
     /**
      * Called when a Web link has been clicked
      *
-     * @param url The uri to the link that has been pressed
+     * @param uri The uri to the link that has been pressed
      */
-    void onClick(Uri url);
+    void onClick(Uri uri);
 }
